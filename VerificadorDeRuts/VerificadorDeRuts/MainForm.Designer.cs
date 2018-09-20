bec7c3b5e0f25dd6b88167af26d612f03735a2dd
M-Aranda <cheloz_20@hotmail.com>
2018-09-19 22:55:18 -0300

    verificador de ruts, aparentemente completo

---
+++ VerificadorDeRuts/VerificadorDeRuts/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Marcelo Aranda
+
+ * Date: 9/18/2018
+ * Time: 11:14 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace VerificadorDeRuts
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btnVerificar;
+		private System.Windows.Forms.Label lblRut;
+		private System.Windows.Forms.TextBox txtRut;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnVerificar = new System.Windows.Forms.Button();
+			this.lblRut = new System.Windows.Forms.Label();
+			this.txtRut = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnVerificar
+			// 
+			this.btnVerificar.Location = new System.Drawing.Point(217, 14);
+			this.btnVerificar.Name = "btnVerificar";
+			this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+			this.btnVerificar.TabIndex = 0;
+			this.btnVerificar.Text = "Verificar";
+			this.btnVerificar.UseVisualStyleBackColor = true;
+			this.btnVerificar.Click += new System.EventHandler(this.BtnVerificarClick);
+			// 
+			// lblRut
+			// 
+			this.lblRut.Location = new System.Drawing.Point(12, 13);
+			this.lblRut.Name = "lblRut";
+			this.lblRut.Size = new System.Drawing.Size(69, 23);
+			this.lblRut.TabIndex = 1;
+			this.lblRut.Text = "RUT:";
+			// 
+			// txtRut
+			// 
+			this.txtRut.Location = new System.Drawing.Point(96, 16);
+			this.txtRut.Name = "txtRut";
+			this.txtRut.Size = new System.Drawing.Size(100, 20);
+			this.txtRut.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(389, 110);
+			this.Controls.Add(this.txtRut);
+			this.Controls.Add(this.lblRut);
+			this.Controls.Add(this.btnVerificar);
+			this.Name = "MainForm";
+			this.Text = "VerificadorDeRuts";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ VerificadorDeRuts/VerificadorDeRuts/MainForm.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Marcelo Aranda
+
+ * Date: 9/18/2018
+ * Time: 11:14 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using VerificadorDeRuts.Utilidades;
+
+namespace VerificadorDeRuts
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		 
+		Util ut;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			ut= new Util();
+			
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void BtnVerificarClick(object sender, EventArgs e)
+		{
+			
+			String rut=txtRut.Text;
+			
+			String digito=ut.GetDigito(rut);
+			Char []r=rut.ToCharArray();
+			MessageBox.Show(r.Last().ToString());
+			Boolean valido=ut.IsRutOk(rut,digito);
+			if(valido==false){
+				MessageBox.Show("El rut ingresado no es valido. El numero verificador debiese ser "+digito);
+			}else if (valido==true){
+				MessageBox.Show("Rut es valido "+digito);
+			}
+			
+		}
+	}
+}
